3a029ea07d84894681aade8e6f892dd2d58e4905
Kenzo Spaulding <jspaulke@jci.com>
2021-06-06 21:56:52 -0700

    Update Utils.js

--- src/login/Utils.js
+++ src/login/Utils.js
@@ -1,5 +1,5 @@
 export async function validateFields(username, password) {
-  let validRE = /[_\-.0-9a-z]/;
+  let validRE = /[^_\-.0-9a-z]+/;
   return !(username.trim().length === 0 || password.trim().length === 0 ||
     username.match(validRE) === null || password.match(validRE) === null);
 
